a6568545a9db2920d44a9618923033255ab7519b
Paul Osman <paul@honeycomb.io>
2021-02-09 16:51:43 -0600

    Create /countries and /countries/id endpoints

--- src/main/java/ca/lifornia/countries/controllers/CountriesController.java
+++ src/main/java/ca/lifornia/countries/controllers/CountriesController.java
@@ -2,19 +2,37 @@ package ca.lifornia.countries.controllers;
 
 import ca.lifornia.countries.models.Country;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
 import java.util.List;
 
 @RestController
 public class CountriesController {
 
-    @RequestMapping(path = "/countries", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    final String allUrl = "http://s3.amazonaws.com/json-countries/countries.json";
+
+    private String getCountryUrl(int id) {
+        return "http://s3.amazonaws.com/json-countries/" + String.valueOf(id) + ".json";
+    }
+
+    @RequestMapping(path = "/countries/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Country getAllCountryById(@PathVariable("id") int id) {
+        final Country country = ClientBuilder.newClient().target(getCountryUrl(id))
+            .request(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+            .get(Country.class);
+        return country;
+    }
+
+    @RequestMapping(path = "/countries", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Country> getAllCountries() {
-        final ArrayList<Country> countryArrayList = new ArrayList<Country>();
-        return countryArrayList;
+        final List<Country> countries = ClientBuilder.newClient().target(allUrl)
+                .request(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+                .get(new GenericType<List<Country>>() {});
+        return countries;
     }
 }
--- src/main/java/ca/lifornia/countries/models/Country.java
+++ src/main/java/ca/lifornia/countries/models/Country.java
@@ -4,6 +4,9 @@ public class Country {
     private int id;
     private String name;
 
+    public Country() {
+
+    }
     public Country(int id, String name) {
         this.id = id;
         this.name = name;
